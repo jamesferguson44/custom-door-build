aca9715b27d35de13b706348beb3af88fcdffa9e
jamesferguson44 <jamesferg44@gmail.com>
2026-07-17 18:04:53 -0600

    Update quote.tsx

--- src/routes/quote.tsx
+++ src/routes/quote.tsx
@@ -156,7 +156,7 @@ function QuotePage() {
                 </div>
                 <div className="divide-y divide-border">
                   {items.map((item) => (
-                    <li
+                    <Div
                       key={item.id}
                       className="px-6 py-5"
                     >
@@ -217,7 +217,7 @@ function QuotePage() {
                           </button>
                         </div>
                       </div>
-                    </li>
+                    </div>
                   ))}
                 </div>
               </div>
